df0a2f99c0fa9c2198c2c1ca7733db1b268c2e56
shueijo <jshue23@gmail.com>
2022-06-20 03:19:58 +1000

    API data

--- reactprojectphase2/src/App.js
+++ reactprojectphase2/src/App.js
@@ -1,19 +1,23 @@
 import React, {useEffect, useState} from "react";
 import './App.css';
-import './keys.js';
+import Recipe from './recipe';
 
 function App() {
+  const APP_ID = '154321d8';
+  const APP_KEY = '660163c905ee13bc11ac96825bd9bdba';
 
-  var url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const [recipes, setRecipes] = useState([]);
 
  useEffect(() =>{
-console.log("Effect has been run");
- }), [];
+   getRecipes();
+ }, []);
 
 const getRecipes = async () => {
-  const response = await fetch (`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
-  const date = response.json();
-  console.log(data);
+  const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+  );
+  const data = await response.json();
+  setRecipes(data.hits);
+  console.log(data.hits);
 }
 
   return (
@@ -22,6 +26,9 @@ const getRecipes = async () => {
        <input className="search-bar" type="text"/>
        <button className="search-button" type="submit">Search</button>
      </form>
+     {recipes.map(recipe =>(
+        <Recipe />
+     ))};
     </div>
   );
 };
